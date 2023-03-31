fbcf4d2d97e97c5dc2901a6dbd41218f614c6eed
iamjayenmthakuri <malla.jayen@gmail.com>
2023-03-31 18:24:53 +0545

    installing axios

--- Hooks/src/Components/EffectTutorial.tsx
+++ Hooks/src/Components/EffectTutorial.tsx
@@ -1,9 +1,15 @@
+import { default as axios } from "axios";
 import { useEffect } from "react";
 
 function EffectTutorial() {
   useEffect(() => {
-    console.log("hellow World");
+    axios
+      .get("https://jsonplaceholder.typicode.com/comments")
+      .then((response: { data: any }) => {
+        console.log(response.data);
+      });
   });
+  console.log("hellow World");
   return <div>EffectTutorial</div>;
 }
 
