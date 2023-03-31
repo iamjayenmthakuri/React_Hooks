e2b9b974cb8c7790a6bd91d518ed115ede82723e
iamjayenmthakuri <malla.jayen@gmail.com>
2023-03-31 18:32:55 +0545

    rest API fetching data

--- Hooks/src/Components/EffectTutorial.tsx
+++ Hooks/src/Components/EffectTutorial.tsx
@@ -1,16 +1,18 @@
 import { default as axios } from "axios";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 function EffectTutorial() {
+  const [data, setData] = useState("");
   useEffect(() => {
     axios
       .get("https://jsonplaceholder.typicode.com/comments")
       .then((response: { data: any }) => {
-        console.log(response.data);
+        setData(response.data[0].email);
+        console.log("API WAS CALLED");
       });
   });
   console.log("hellow World");
-  return <div>EffectTutorial</div>;
+  return <div>{data}</div>;
 }
 
 export default EffectTutorial;
