cddb11e8a9c68c29412d616a58555a01109e5251
iamjayenmthakuri <malla.jayen@gmail.com>
2023-03-31 16:49:56 +0545

    commiting first

---
+++ Hooks/src/Components/StateTutorial.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function StateTutorial() {
+  let counter = 0;
+  return (
+    <div>
+      {counter} <button>Increment</button>
+    </div>
+  );
+}
+
+export default StateTutorial;
---
+++ Hooks/src/main.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import StateTutorial from "./Components/StateTutorial";
+import "./index.css";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+  <React.StrictMode>
+    <StateTutorial />
+  </React.StrictMode>
+);
