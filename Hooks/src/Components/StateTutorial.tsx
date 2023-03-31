4920838d490e6fc46bd3093b5fdfe98eb9fb022a
iamjayenmthakuri <malla.jayen@gmail.com>
2023-03-31 17:13:47 +0545

    finishing useState

--- Hooks/src/Components/StateTutorial.tsx
+++ Hooks/src/Components/StateTutorial.tsx
@@ -6,11 +6,21 @@ function StateTutorial() {
   const Increment = () => {
     setCounter(counter + 1);
   };
+  const [inputValue, setInputValue] = useState("Jayen");
+
+  let change = (event: { target: { value: any } }) => {
+    const newValue = event.target.value;
+    setInputValue(newValue);
+  };
 
   return (
     <>
       {counter}
       <button onClick={Increment}>Increment</button>
+      <div>
+        <input type="text" placeholder="enter-something..." onChange={change} />
+        {inputValue}
+      </div>
     </>
   );
 }
