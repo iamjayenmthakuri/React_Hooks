0e41d1518c361d0c125c5e296cc3471364e7c847
iamjayenmthakuri <malla.jayen@gmail.com>
2023-03-31 18:07:09 +0545

    starting useEffect

---
+++ Hooks/src/Components/EffectTutorial.tsx
@@ -0,0 +1,10 @@
+import { useEffect } from "react";
+
+function EffectTutorial() {
+  useEffect(() => {
+    console.log("hellow World");
+  });
+  return <div>EffectTutorial</div>;
+}
+
+export default EffectTutorial;
--- Hooks/src/main.tsx
+++ Hooks/src/main.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import EffectTutorial from "./Components/EffectTutorial";
 import ReducerTutorial from "./Components/ReducerTutorial";
 import StateTutorial from "./Components/StateTutorial";
 import "./index.css";
@@ -8,5 +9,6 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
   <React.StrictMode>
     <StateTutorial />
     <ReducerTutorial />
+    <EffectTutorial />
   </React.StrictMode>
 );
