31cb15da45536d7a13b61de823864d537398c2cc
iamjayenmthakuri <malla.jayen@gmail.com>
2023-04-02 14:40:38 +0545

    ContextApi ---

---
+++ Hooks/src/Components/ContextApi/User.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function User() {
+  return <div>USer</div>;
+}
+
+export default User;
---
+++ Hooks/src/Components/ContextApi/contextTutorials.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import Login from "./Login";
+import User from "./User";
+
+function contextTutorials() {
+  return <div>contextTutorials</div>;
+}
+
+export default contextTutorials;
